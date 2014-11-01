08ca42834bacde2f9f0a323964f18b4020188bc2
Antoine <antoine@van-elstraete.net>
2014-11-01 15:21:02 +0100

    Update TlsSniSocketFactory.java

--- src/at/bitfire/davdroid/mirakel/webdav/TlsSniSocketFactory.java
+++ src/at/bitfire/davdroid/mirakel/webdav/TlsSniSocketFactory.java
@@ -4,8 +4,12 @@
  * are made available under the terms of the GNU Public License v3.0
  * which accompanies this distribution, and is available at
  * http://www.gnu.org/licenses/gpl.html
+- * 	
+- * Contributors: 	
+- * Richard Hirner (bitfire web engineering) - initial API and implementation
  ******************************************************************************/
-package at.bitfire.davdroid.webdav;
+
+package at.bitfire.davdroid.mirakel.webdav;
 
 import java.io.IOException;
 import java.net.InetSocketAddress;
